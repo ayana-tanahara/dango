5e7f0bc1622553089600f0d800f739ffb8749b32
ayana-tanahara <goodtasete@gmail.com>
2023-01-20 02:54:04 +0900

    テスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,152 @@ const IndexPage: NextPage = (): ReactElement => {
         <div className="p-3 mb-3 border-2 rounded h-full w-full text-right">
           <span className="text-gray-700 select-none">{count}</span>
         </div>
-        <div className="grid grid-cols-3 gap-2">
+        <div className="grid grid-cols-4 gap-2">
+
+        <Button
+            className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              setCount(0);
+            }}>
+            <span className="select-none text-xl">AC</span>
+          </Button>
+
+          <Button
+            className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              setCount(0);
+            }}>
+            <span className="select-none text-xl">±</span>
+          </Button>
+
+          <Button
+            className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              setCount(0);
+            }}>
+            <span className="select-none text-xl">%</span>
+          </Button>
+
+          <Button
+            className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              setCount(0);
+            }}>
+            <span className="select-none text-xl">÷</span>
+          </Button>
+
+        <Button
+            className="py-2 bg-cyan-000 text-Black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count + 7);
+            }}>
+            <span className="select-none text-xl">7</span>
+          </Button>
+
+          <Button
+            className="py-2 bg-cyan-000 text-Black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count + 8);
+            }}>
+            <span className="select-none text-xl">8</span>
+          </Button>
+
+          <Button
+            className="py-2 bg-cyan-000 text-Black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count + 9);
+            }}>
+            <span className="select-none text-xl">9</span>
+          </Button>
+
+
+          <Button
+            className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              setCount(0);
+            }}>
+            <span className="select-none text-xl">×</span>
+          </Button>
+
+
+          <Button
+            className="py-2 bg-cyan-000 text-Black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count + 4);
+            }}>
+            <span className="select-none text-xl">4</span>
+          </Button>
+
+          <Button
+            className="py-2 bg-cyan-000 text-Black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count + 5);
+            }}>
+            <span className="select-none text-xl">5</span>
+          </Button>
+
+          <Button
+            className="py-2 bg-cyan-)000 text-black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count + 6);
+            }}>
+            <span className="select-none text-xl">6</span>
+          </Button>
+
+
+          <Button
+            className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              setCount(count / n);
+            }}>
+            <span className="select-none text-xl">-</span>
+          </Button>
+
+
+          <Button
+            className="py-2 bg-cyan-)000 text-black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count + 1);
+            }}>
+            <span className="select-none text-xl">1</span>
+          </Button>         
+
+
+          <Button
+            className="py-2 bg-cyan-)000 text-black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count + 2);
+            }}>
+            <span className="select-none text-xl">2</span>
+          </Button>    
+
+
+          <Button
+            className="py-2 bg-cyan-)000 text-black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count + 3);
+            }}>
+            <span className="select-none text-xl">3</span>
+          </Button>    
+
           <Button
             className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
             onClick={() => {
@@ -19,17 +164,30 @@ const IndexPage: NextPage = (): ReactElement => {
 
               setCount(count + 1);
             }}>
-            <span className="select-none text-xl">+</span>
+            <span className="select-none text-xl">-</span>
           </Button>
+
+
           <Button
-            className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+            className="py-2 bg-cyan-)000 text-black rounded border border-gray-200 cursor-pointer"
             onClick={() => {
               console.log(count);
 
-              setCount(count - 1);
+              setCount(count + 0);
             }}>
-            <span className="select-none text-xl">-</span>
-          </Button>
+            <span className="select-none text-xl">0</span>
+          </Button>    
+
+          <Button
+            className="py-2 bg-cyan-)000 text-black rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(0);
+            }}>
+            <span className="select-none text-xl">.</span>
+          </Button>  
+
           <Button
             className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
             onClick={() => {
@@ -37,6 +195,18 @@ const IndexPage: NextPage = (): ReactElement => {
             }}>
             <span className="select-none text-xl">C</span>
           </Button>
+
+          <Button
+            className="py-2 bg-cyan-600 text-white rounded border border-gray-200 cursor-pointer"
+            onClick={() => {
+              console.log(count);
+
+              setCount(count - 1);
+            }}>
+            <span className="select-none text-xl">=</span>
+          </Button>
+
+
         </div>
       </div>
     </div>
